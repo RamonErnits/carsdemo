6d66d6764144b5685d8252ac6dee57c13a65c78b
Opilane TTHK <opilane@tthk.ee>
2022-09-09 15:14:12 +0300

    As an app developer I want to get a list of all car ads to show them in my app[#183186672]

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const app = require('express')();
 const port = 8080;
 
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swagger.json');
+const swaggerDocument = require('./docs/swaggers.json');
 
+app.get('/cars', (req, res) => {
+    res.send('Audi a5, BMW 3, Mercedes c' ); 
+});
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
     console.log(`API up at http://localhost:${port}`)
-    }
-);
+});
